5480a73ff1e6d55c415ba01033452258cb250836
disegrafcode <disegrafcode@gmail.com>
2021-12-15 14:38:47 -0500

    15-12-2021

--- assets/lib/verticalAccordeon/js/verticalAccordeon.js
+++ assets/lib/verticalAccordeon/js/verticalAccordeon.js
@@ -1,15 +1,27 @@
+let objAnterior = undefined;
+let closing = false;
+
 function revealContent(obj)
 {
-    addClassInactiveItems();
     let target = obj;
-    target.classList.remove("itemAccordeonInactive");
-    target.classList.add("activeItemAcordeon");
+    if(closing)
+    {
+        return;
+    }
+    if(!target.classList.contains("activeItemAcordeon"))
+    {
+        addClassInactiveItems();
+        exitActiveItem(objAnterior);
+
+        target.classList.remove("itemAccordeonInactive");
+        target.classList.add("activeItemAcordeon");
+        objAnterior = obj;
+    }
 }
 
 
 function addClassInactiveItems()
 {
-   
     let parent = document.getElementById("accordeon1");
     let items = parent.querySelectorAll(".itemAccordeon");
     for(let i=0; i<items.length;i++)
@@ -17,3 +29,40 @@ function addClassInactiveItems()
         items[i].classList.add("itemAccordeonInactive");
     }
 }
+
+function exitActiveItem(obj)
+{
+    if(obj!=undefined)
+    {
+        obj.classList.remove("activeItemAcordeon");
+        obj.classList.add("exitAccordeonAnimation");
+
+        /* END ANIMATION */
+        obj.addEventListener("webkitAnimationEnd", function(){
+            obj.classList.remove("exitAccordeonAnimation");
+        });
+        obj.addEventListener("animationend", function(){
+            obj.classList.remove("exitAccordeonAnimation");
+        });
+        /* END ANIMATION */
+    }
+}
+
+function restarAllItems()
+{
+    closing = true;
+    let parent = document.getElementById("accordeon1");
+    let items = parent.querySelectorAll(".itemAccordeon");
+    for(let i=0; i<items.length;i++)
+    {
+        items[i].classList.remove("activeItemAcordeon");
+        items[i].classList.remove("itemAccordeonInactive");
+        items[i].classList.remove("exitAccordeonAnimation");
+    }
+    objAnterior = undefined;
+
+    setTimeout(function () {
+        closing = false;
+    }, 1000);
+}
+
